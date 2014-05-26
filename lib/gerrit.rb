6714699df1ec7aebbb3f92f02443e94ed4042810
Jun Wu <quark@lihdd.net>
2014-05-26 17:16:08 +0800

    Add lib/gerrit

---
+++ lib/gerrit.rb
@@ -0,0 +1,28 @@
+require 'httpi'
+require 'json'
+require 'base64'
+
+class Gerrit < Struct.new(:base_url, :username, :password)
+
+  def get endpoint
+    req = HTTPI::Request.new File.join(base_url, username ? 'a' : '', endpoint)
+    req.auth.digest username, password
+    req.auth.ssl.verify_mode = :none
+    res = HTTPI.get req
+    content_type = [*res.headers['Content-Type']].last.split(/[ ;]/).first
+    case content_type
+    when 'application/json'
+      JSON.parse res.body.lines[1..-1].join
+    when 'text/plain'
+      case res.headers['X-FYI-Content-Encoding']
+      when 'base64'
+        Base64::decode64 res.body
+      else
+        res.body
+      end
+    else
+      raise "Unknown content type: #{content_type}"
+    end
+  end
+
+end
